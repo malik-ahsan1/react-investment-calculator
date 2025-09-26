62cf81093f885dee025b904a47b9ed0a7a611af2
Malik M. Ahsan <malikmuhammadahsan53@gmail.com>
2025-09-26 05:21:32 +0500

    feat: implement Results component with investment calculation table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from 'react';
 import Header from './components/Header';
+import Results from './components/Results';
 import UserInput from './components/UserInput';
+import { calculateInvestmentResults } from './util/investment';
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 2000,
+    expectedReturn: 6,
+    duration: 7,
+  });
+
+  const isInputValid = userInput.duration >= 1;
+
+  function handleValueChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => ({
+      ...prevUserInput,
+      [inputIdentifier]: +newValue,
+    }));
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} handleValueChange={handleValueChange} />
+      {isInputValid ? (
+        <Results userInput={userInput} />
+      ) : (
+        <p className="center">Please enter duration greater than zero!</p>
+      )}
     </>
   );
 }
